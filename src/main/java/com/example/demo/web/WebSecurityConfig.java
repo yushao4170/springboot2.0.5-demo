f0ff11d161137a4756e1139212a3b3872f8dfbaa
黄国渝 <guoyu.huang@xmasq.com>
2018-09-28 15:31:57 +0800

    去除不必要的配置

--- src/main/java/com/example/demo/web/WebSecurityConfig.java
+++ src/main/java/com/example/demo/web/WebSecurityConfig.java
@@ -1,9 +1,7 @@
 package com.example.demo.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -35,9 +33,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.userDetailsService(userDetailsService);
 	}
 
-	@Bean
-	@Override
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
 }
